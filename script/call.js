5c6ebe02f8c69c49ca1001a8dcd4e38b3e118c53
1215kkm <1215kkm@naver.com>
2023-10-31 14:04:21 +0900

    github 내부 js로 수정

--- script/call.js
+++ script/call.js
@@ -1,7 +1,5 @@
-alert('k')
-
-
 $(document).ready(function(){
-    alert('k')
-    $('.teacher').html('성남 그린컴퓨터 강쌤반 <a href="tel:010-3321-7317"><i class="fa-solid fa-square-phone"></i> 질문 010-3321-7317</a>');;
+    $('body').prepend('<div class="teacher">성남 그린컴퓨터 강쌤반 <a href="tel:010-3321-7317"><i class="fa-solid fa-square-phone"></i> 질문 010-3321-7317</a></div>')
+    $('.teacher').css({position: 'fixed', width: '100%', height: '50px', background:'rgba(64,108,178,1)', zIndex: '777777', bottom: '0%', display: 'flex', justifyContent:' center', alignItems: 'center', color:' #fff'})
+    $('.teacher a').css({color: '#e9ff7b', marginLeft: '30px'})
 })
